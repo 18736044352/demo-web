805add1e62a7cd3e8e04858fc3078be9bcf873f2
jingchao.zhu <jingchao.zhu@ishansong.com>
2018-01-19 16:11:27 +0800

    dubbo

--- src/main/java/com/java/controller/DubboController.java
+++ src/main/java/com/java/controller/DubboController.java
@@ -1,7 +1,10 @@
 package com.java.controller;
 
+import com.java.interfaces.DubboInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by iss on 18/1/18.
@@ -10,8 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("dubbo/")
 public class DubboController {
 
+    @Autowired
+    private DubboInterface dubboService;
+
+    @ResponseBody
     @RequestMapping("/getMessage")
     public String getMessage(){
+        System.out.println(dubboService.getDubboDto("hello"));
         return "";
     }
 }
